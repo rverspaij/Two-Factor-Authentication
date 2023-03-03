8d4cd8372c0640f83d0ee5306ea26a4be75e0d8b
Raoul <r.verspaij@student.fontys.nl>
2023-03-03 13:42:10 +0100

    test69

--- main.go
+++ main.go
@@ -1,52 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-const htmlLogin = `
-<html>
-<body>
-	<form action="/login" method="POST">
-		<label for="username">Username:</label><br>
-		<input type="text" id="username" name="username"><br>
-		<label for="password">Password:</label><br>
-		<input type="password" id="password" name="password"><br><br>
-		<input type="submit" value="Submit">
-	</form> 
-</body>
-</html>
-`
-
-func login(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		// Get the username and password from the form
-		username := r.FormValue("user")
-		password := r.FormValue("password")
-
-		checker := checkLogin(username, password)
-		// Check the username and password against a database or other method
-		if checker {
-			// If the login is successful, redirect the user to the dashboard page
-			http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
-		} else {
-			// If the login is unsuccessful, display an error message
-			fmt.Fprint(w, "Incorrect username or password. Please try again.")
-		}
-	} else {
-		// If the request is not a POST request, display the login form
-		fmt.Fprint(w, htmlLogin)
-	}
-}
-
-func checkLogin(username string, password string) bool {
-	correct := true
-	return correct
-
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/login", login)
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Hello World!")
 }
